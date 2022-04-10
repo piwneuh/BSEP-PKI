c0246f46dd4e4d02fd2425e5e3248f82094520e3
piwneuh <filip.pinjuh@gmail.com>
2022-04-11 00:56:55 +0200

    front login

--- bsep/src/main/java/com/bsep/controller/LoginController.java
+++ bsep/src/main/java/com/bsep/controller/LoginController.java
@@ -6,13 +6,10 @@ import com.bsep.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/login")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class LoginController {
 
     @Autowired
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminHomePageComponent } from './admin-home-page/admin-home-page.component';
+import { ClientHomePageComponent } from './client-home-page/client-home-page.component';
 import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [{path:'login', component: LoginComponent}];
+const routes: Routes = [
+  {path:'login', component: LoginComponent},
+  {path:'admin', component: AdminHomePageComponent},
+  {path:'client', component: ClientHomePageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
